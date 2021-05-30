f6e713170bb1c526a61a692725d1404037fedc2d
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-05-31 01:22:22 +0300

    Add another test isNotEmptyInput

--- src/main/java/models/Ceasar.java
+++ src/main/java/models/Ceasar.java
@@ -1,13 +1,22 @@
 package models;
 
 public class Ceasar {
-    private int mkey;
-    private String mText;
+    private int key;
+    private String Text;
     public void isEncoded(int key, String Text){
-        mkey=key;
-        mText=Text;
+        this.key=key;
+        this.Text=Text;
     }
-   public  boolean isEncoded(String Text){
-       return true;
+//   public  boolean isEncoded(String Text){
+//       return true;
+//   }
+    public boolean isNotEmpty(){
+        return !Text.trim().isEmpty();
+    }
+    public String getText(){
+        return Text;
+    }
+   public int getKey(){
+        return key;
    }
 }
--- src/test/java/models/CeasarTest.java
+++ src/test/java/models/CeasarTest.java
@@ -8,18 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CeasarTest {
     @Test
+    public void isEncoded_notEmptyInput(){
+        Ceasar testCeasar=new Ceasar();
+        assertEquals(true, testCeasar.isNotEmpty());
+    }
+
+    @Test
     public void isEncoded_checkInstances(){
         Ceasar testCeasar=new Ceasar();
-        assertEquals(true, testCeasar instanceof isEncoded);
+        assertEquals(true, testCeasar instanceof Ceasar);
     }
     @Test
    public void isEncoded_inputIsString_true() {
         Ceasar testCeasar=new Ceasar();
-        assertEquals(true, testCeasar.isEncoded(""));
+        assertEquals("",testCeasar.getText());
     }
-//     @Test
-//    public void isEncoded_cipherKey_int(){
-//         Ceasar testCeasar=new Ceasar();
-//         assertEquals(testCeasar.isEncoded(2, ""));
-//     }
+     @Test
+    public void isEncoded_cipherKey_int(){
+         Ceasar testCeasar=new Ceasar();
+         assertEquals(2,testCeasar.getKey());
+     }
 }
