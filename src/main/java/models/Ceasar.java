4633d047d1ea87dbc168ed577b58e78091e9a09d
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-05-31 01:51:02 +0300

    Add the implement code for test validEncoding

--- src/main/java/models/Ceasar.java
+++ src/main/java/models/Ceasar.java
@@ -32,4 +32,24 @@ public class Ceasar {
    public boolean isValidKey(){
        return key >1 && key < 26;
    }
+   public String validEncoding(){
+       String encoded = "";
+       String encodedArray[] = Text.split("");
+       String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       for (int i = 0; i<encodedArray.length;i++)
+       {
+           if (Text.charAt(i) == ' ')
+           {
+               encoded += " ";
+           }
+           else
+           {
+               int charPosition = alphabet.indexOf(Text.charAt(i));
+               int Key = (key + charPosition) % 26;
+               encoded += alphabet.charAt(key);
+           }
+       }
+       return encoded.toUpperCase();
+   }
 }
+
